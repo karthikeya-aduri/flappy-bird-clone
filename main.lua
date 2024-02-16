0776bcc89f6105f820db59398ecc16ebcba81e0a
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-02-16 19:43:59 +0530

    Infinte scrolling of pipe pairs is added with this commit

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 Push = require('push')
 Class = require('class')
 require('Bird')
+require('Pipe')
+require('PipePair')
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -18,8 +20,17 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+local GROUND_LOOPING_POINT = 514
 
 local bird = Bird()
+
+local pipePairs = {}
+
+local spawnTimer = 0
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+local timerReset = 2
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -27,9 +38,9 @@ function love.load()
     Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
-        resizable = true,
-        borderless = false
+        resizable = true
     })
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -37,20 +48,54 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
     if key=='escape' then
         love.event.quit()
     end
 end
 
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
 function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED*dt) % BACKGROUND_LOOPING_POINT
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED*dt) % VIRTUAL_WIDTH
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED*dt) % GROUND_LOOPING_POINT
+
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer>timerReset then
+        timerReset = math.random(2, 3)
+        local y = math.max(-PIPE_HEIGHT+10, math.min(lastY+math.random(-20, 20), VIRTUAL_HEIGHT-90-PIPE_HEIGHT))
+        lastY = y;
+        table.insert(pipePairs, PipePair(y))
+        spawnTimer = 0
+    end
+
+    bird:update(dt)
+
+    for k, pair in pairs(pipePairs) do
+        pair:update(dt)
+    end
+
+    for k, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, k)
+        end
+    end
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
     Push:start()
-    love.graphics.draw(background, 0-backgroundScroll, 0)
-    love.graphics.draw(ground, 0-groundScroll, VIRTUAL_HEIGHT-16)
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    for k, pair in pairs(pipePairs) do
+        pair:render()
+    end
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
     bird:render()
     Push:finish()
 end
