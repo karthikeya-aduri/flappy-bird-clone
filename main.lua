8712ecd7320454bac27fe052e601ec8625be22f3
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-02-16 13:58:20 +0530

    1. Added scrolling to the ground and background 2. Edited the ground.png

--- main.lua
+++ main.lua
@@ -1,22 +1,35 @@
 Push = require('push')
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle('Flappy Bird Clone')
     Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
-        fullscreen = true,
-        resizable = false
+        fullscreen = false,
+        resizable = true,
+        borderless = false
     })
+end
+
+function love.resize(w, h)
+    Push:resize(w,h)
 end
 
 function love.keypressed(key)
@@ -25,9 +38,14 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED*dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED*dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     Push:start()
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT-16)
+    love.graphics.draw(background, 0-backgroundScroll, 0)
+    love.graphics.draw(ground, 0-groundScroll, VIRTUAL_HEIGHT-16)
     Push:finish()
 end
