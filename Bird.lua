2e4b88ee9389f80a092ef49079fd1ba0dd82d22d
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-02-16 19:42:29 +0530

    Added gravity to the game

--- Bird.lua
+++ Bird.lua
@@ -1,5 +1,7 @@
 Bird = Class{}
 
+local GRAVITY = 20
+
 function Bird:init()
     self.image = love.graphics.newImage('bird.png')
     self.width = self.image:getWidth()
@@ -7,6 +9,16 @@ function Bird:init()
 
     self.x = (VIRTUAL_WIDTH - self.width )/2
     self.y = (VIRTUAL_HEIGHT - self.height )/2
+
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + (GRAVITY*dt)
+    if (love.keyboard.keysPressed['space']) then
+        self.dy = -5
+    end
+    self.y = self.y + self.dy
 end
 
 function Bird:render()
