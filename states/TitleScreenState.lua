0fd1b96e5e9c19bf06dd57465759cf3bfbee35c8
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-02-17 10:10:45 +0530

    Changed the text on title screen

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -11,5 +11,6 @@ function TitleScreenState:render()
     love.graphics.printf({{0, 0, 0}, 'Flappy Bird Clone'}, 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(MediumFont)
-    love.graphics.printf({{0, 0, 0}, 'Press Enter to play, Esc to exit at any point'}, 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf({{0, 0, 0}, 'Press Enter to play'}, 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf({{0, 0, 0}, 'Contols: Esc=Exit, Space=Jump'}, 0, 126, VIRTUAL_WIDTH, 'center')
 end
